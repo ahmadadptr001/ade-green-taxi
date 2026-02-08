8b50848ee1559bbcfec55998f8cc4fed19cb48f5
Ahmad Adptr <ahmadadptr@gmail.com>
2026-02-09 03:09:00 +0800

    ngubah customer sidebar trigger nya agar sticky

--- components/dashboard/CustomSidebarTrigger.jsx
+++ components/dashboard/CustomSidebarTrigger.jsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export default function CustomSidebarTrigger() {
   return (
-    <header className="flex items-center gap-2 w-full bg-white">
+    <header className="flex items-center gap-2 w-full bg-white sticky top-0">
       <SidebarTrigger />
       <p className="font-bold sm:text-sm line-clamp-1 text-[.7rem]">Tekan untuk menutup/membuka sidebar</p>
       <div className="flex items-center p-0 px-2 sm:px-3 gap-2 rounded-full bg-gray-200 sm:p-1">
