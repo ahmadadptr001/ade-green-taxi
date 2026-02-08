be90a131058162bd2f2ddf7bc1b551c2f76dd3a5
Ahmad Adptr <ahmadadptr@gmail.com>
2026-02-09 02:53:45 +0800

    besarin dikit teks nya

--- components/dashboard/CustomSidebarTrigger.jsx
+++ components/dashboard/CustomSidebarTrigger.jsx
@@ -5,10 +5,10 @@ export default function CustomSidebarTrigger() {
   return (
     <header className="flex items-center gap-2 w-full bg-white">
       <SidebarTrigger />
-      <p className="font-bold sm:text-sm line-clamp-1 text-[.65rem]">Tekan untuk menutup/membuka sidebar</p>
+      <p className="font-bold sm:text-sm line-clamp-1 text-[.7rem]">Tekan untuk menutup/membuka sidebar</p>
       <div className="flex items-center p-0 px-2 sm:px-3 gap-2 rounded-full bg-gray-200 sm:p-1">
       <Command className="w-2 h-2 sm:w-4 sm:h-4"/>
-      <span className="font-bold text-[.65rem] sm:text-sm">+ B</span>
+      <span className="font-bold text-[.7rem] sm:text-sm">+ B</span>
       </div>
     </header>
   )
